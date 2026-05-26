8df8d628ab7b0ec7c7e1e7093e6bf5d84f563449
NMS\longdt <longdt1@nms.com.vn>
2026-05-26 11:16:29 +0700

    add: authen bearer in swagger

--- src/main/java/com/nms/evaluation/config/OpenApiConfig.java
+++ src/main/java/com/nms/evaluation/config/OpenApiConfig.java
@@ -1,14 +1,19 @@
 package com.nms.evaluation.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "Bearer Authentication";
 
     @Bean
     public OpenAPI customOpenAPI() {
@@ -23,6 +28,14 @@ public class OpenApiConfig {
                                 .url("https://nms.com"))
                         .license(new License()
                                 .name("Apache 2.0")
-                                .url("https://springdoc.org")));
+                                .url("https://springdoc.org")))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
+                                .name(SECURITY_SCHEME_NAME)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("Nhập JWT token để xác thực. Ví dụ: eyJhbGciOiJSUzI1NiIs...")));
     }
 }
